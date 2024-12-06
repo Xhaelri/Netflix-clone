e4e27db7bd3a64b3a138e5bde071d8f99534d9d0
Ali Saleh <156487348+Xhaelri@users.noreply.github.com>
2024-12-06 12:53:35 +0200

    commit

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -4,37 +4,44 @@ import back_arrow_icon from "../../assets/back_arrow_icon.png";
 import { useNavigate, useParams } from "react-router-dom";
 
 const Player = () => {
-  
-  const {id} = useParams();
+  const { id } = useParams();
   const navigate = useNavigate();
 
-
-  const [apiData,seApiData]= useState({
+  const [apiData, seApiData] = useState({
     name: "",
     key: "",
     published_at: "",
-    type: ""
-  })
+    type: "",
+  });
 
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0M2VkNjY5OWE1YWM2ZTBjMGNjNTgxYzU0NDUzNjhjZiIsIm5iZiI6MTczMjk2NTM5Ni4wMTYsInN1YiI6IjY3NGFmNDE0NTFkMjZiYTkzYzc4YTgxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fru0sPK5oZeUaxuMbeEPGoyOwL1BjfAhvBc2ZlDjwi4'
-    }
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0M2VkNjY5OWE1YWM2ZTBjMGNjNTgxYzU0NDUzNjhjZiIsIm5iZiI6MTczMjk2NTM5Ni4wMTYsInN1YiI6IjY3NGFmNDE0NTFkMjZiYTkzYzc4YTgxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fru0sPK5oZeUaxuMbeEPGoyOwL1BjfAhvBc2ZlDjwi4",
+    },
   };
-  
-  useEffect(()=>{
-    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
-      .then(res => res.json())
-      .then(res => seApiData(res.results[0]))
-      .catch(err => console.error(err));
-  },[])
 
-  
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => seApiData(res.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="player">
-      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-1)}}/>
+      <img
+        src={back_arrow_icon}
+        alt=""
+        onClick={() => {
+          navigate(-2);
+        }}
+      />
       <iframe
         title="trailer"
         frameBorder="0"
@@ -44,7 +51,7 @@ const Player = () => {
         height="90%"
       ></iframe>
       <div className="player-info">
-        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.published_at.slice(0, 10)}</p>
         <p>{apiData.name}</p>
         <p>{apiData.type}</p>
       </div>
